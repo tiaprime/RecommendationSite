f3ec53bd2c7859dc725f38c502ef0677b0eb9fae
amymmcd <amymargaret22@gmail.com>
2025-04-04 15:12:25 -0600

    backend commit

--- backend/RecommendationSite.API/obj/Debug/net8.0/RecommendationSite.API.AssemblyInfo.cs
+++ backend/RecommendationSite.API/obj/Debug/net8.0/RecommendationSite.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecommendationSite.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9d8cda1eeea56ca0f38930e85be3c0430332bb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+264930dbba76cf7592274aeb8683f1bff20a5c60")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecommendationSite.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecommendationSite.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
